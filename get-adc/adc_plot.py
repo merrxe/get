5a6b82c790913cd927103915ad912a2b6a89f688
merrxe <qmary2007q@gmail.com>
2026-05-20 19:55:27 +0300

    Update adc_plot.py

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -1,22 +1,17 @@
-import time
-from r2r_adc import R2R_ADC
-import adc_plot
+import matplotlib.pyplot as plt
 
-voltage_values = []
-time_values = []
-duration = 3.0
-
-try:
-    adc = R2R_ADC(3.3, compare_time=0.0001, verbose=False)
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10,6))
+    plt.plot(time, voltage)
     
-    start_time = time.time()
+    plt.title("Зависимость напряжения от времени", fontsize=16, fontweight="bold")
+    plt.xlabel("Время, с", fontsize=12)
+    plt.ylabel("Напряжение, В", fontsize=12)
     
-    while (time.time() - start_time) < duration:
-        voltage = adc.get_sc_voltage()
-        voltage_values.append(voltage)
-        time_values.append(time.time() - start_time)
+    plt.xlim(0, max(time) + 1)
+    plt.ylim(0, max_voltage)
     
-    adc_plot.plot_voltage_vs_time(time_values, voltage_values, 3.3)
+    plt.grid(True, alpha=0.3, linestyle="--")
     
-finally:
-    adc.deinit()
+    plt.tight_layout()
+    plt.show()
