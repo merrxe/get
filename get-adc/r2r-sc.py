a3f1c732e3706678f562da7b5da8d1f00a568633
merrxe <qmary2007q@gmail.com>
2026-05-20 19:27:06 +0300

    Update r2r-sc.py

--- get-adc/r2r-sc.py
+++ get-adc/r2r-sc.py
@@ -1,27 +1,58 @@
+import RPi.GPIO as GPIO
 import time
-from r2r_adc import R2R_ADC
-import adc_plot
 
-voltage_values = []
-time_values = []
-duration = 3.0
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time=0.01, verbose=False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.compare_time = compare_time
+        
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
 
-try:
-    adc = R2R_ADC(3.3, compare_time=0.0001, verbose=False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial=0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
     
-    start_time = time.time()
+    def deinit(self):
+        GPIO.output(self.bits_gpio, 0)
+        GPIO.cleanup()
     
-    while (time.time() - start_time) < duration:
-        voltage = adc.get_sar_voltage()  # используем метод ПП
-        voltage_values.append(voltage)
-        time_values.append(time.time() - start_time)
+    def number_to_dac(self, number):
+        binary = [int(bit) for bit in bin(number)[2:].zfill(8)]
+        for i in range(8):
+            GPIO.output(self.bits_gpio[i], binary[i])
     
-    # Строим график
-    adc_plot.plot_voltage_vs_time(time_values, voltage_values, 3.3)
+    def sequential_counting_adc(self):
+        for number in range(256):
+            self.number_to_dac(number)
+            time.sleep(self.compare_time)
+            
+            if GPIO.input(self.comp_gpio) == GPIO.HIGH:
+                return number
+        return 255
     
-    # Строим гистограмму (если функция есть в adc_plot)
-    if hasattr(adc_plot, 'plot_sampling_period_hist'):
-        adc_plot.plot_sampling_period_hist(time_values)
+    def get_sc_voltage(self):
+        value = self.sequential_counting_adc()
+        voltage = (value / 256) * self.dynamic_range
+        if self.verbose:
+            print(f"Напряжение (ПС): {voltage:.3f}В")
+        return voltage
     
-finally:
-    adc.deinit()
+    def successive_approximation_adc(self):
+        result = 0
+        for bit in range(7, -1, -1):
+            test_value = result | (1 << bit)
+            self.number_to_dac(test_value)
+            time.sleep(self.compare_time)
+            
+            if GPIO.input(self.comp_gpio) == GPIO.HIGH:
+                result = test_value
+        return result
+    
+    def get_sar_voltage(self):
+        value = self.successive_approximation_adc()
+        voltage = (value / 256) * self.dynamic_range
+        if self.verbose:
+            print(f"Напряжение (ПП): {voltage:.3f}В")
+        return voltage
